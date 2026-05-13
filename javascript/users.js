c9d213a26a90ab2c156dbc92048f9e49425bca82
belmanal <manalbel710@gmail.com>
2026-05-13 15:59:16 +0100

    Update users.js

--- javascript/users.js
+++ javascript/users.js
@@ -5,6 +5,26 @@ const users = [
         password: "Manal123"
     },
     {
+        name: "Leticia",
+        email: "leticia@gmail.com",
+        password: "Leticia123"
+    },
+    {
+        name: "Kenza",
+        email: "kenza@gmail.com",
+        password: "Kenza123"
+    },
+    {
+        name: "Salma",
+        email: "salma@gmail.com",
+        password: "Salma123"
+    },
+    {
+        name: "Lamia",
+        email: "lamia@gmail.com",
+        password: "Lamia123"
+    },
+    {
         name: "Admin",
         email: "admin@gmail.com",
         password: "Admin123"
