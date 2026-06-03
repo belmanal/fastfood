a86e4ae1e3e75b108baf6b72612ecff40552ef67
belmanal <manalbel710@gmail.com>
2026-06-04 00:31:54 +0100

    Update box.js

--- javascript/box.js
+++ javascript/box.js
@@ -23,24 +23,48 @@ function updatePrice() {
 
 
 function createBox() {
-    const main = document.getElementById("main").value;
-    const side = document.getElementById("side").value;
-    const drink = document.getElementById("drink").value;
-    const dessert = document.getElementById("dessert").value;
 
-    const totalText = document.getElementById("totalPrice").textContent;
+    const main = document.getElementById("main");
+    const side = document.getElementById("side");
+    const drink = document.getElementById("drink");
+    const dessert = document.getElementById("dessert");
 
     const result = document.getElementById("resultBox");
 
+    if (!main.value || !side.value || !drink.value || !dessert.value) {
+        result.innerHTML = `<div class="error-box">⚠️ Veuillez tout choisir</div>`;
+        return;
+    }
+
+    const total =
+        Number(main.options[main.selectedIndex].dataset.price) +
+        Number(side.options[side.selectedIndex].dataset.price) +
+        Number(drink.options[drink.selectedIndex].dataset.price) +
+        Number(dessert.options[dessert.selectedIndex].dataset.price);
+
+    const box = {
+        name: `Box (${main.value}, ${side.value}, ${drink.value}, ${dessert.value})`,
+        price: total,
+        qty: 1
+    };
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    let index = cart.findIndex(item => item.name === box.name);
+
+    if (index !== -1) {
+        cart[index].qty += 1;
+    } else {
+        cart.push(box);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
     result.innerHTML = `
         <div class="success-box">
-            <h3>✔️ Box créée avec succès</h3>
-            <p><strong>🍔 Plat :</strong> ${main}</p>
-            <p><strong>🍟 Accompagnement :</strong> ${side}</p>
-            <p><strong>🥤 Boisson :</strong> ${drink}</p>
-            <p><strong>🍰 Dessert :</strong> ${dessert}</p>
-            <h4>${totalText}</h4>
+            <h3>Box créée avec succès</h3>
+            <p>${box.name}</p>
+            <h2>${total} DA</h2>
         </div>
     `;
-   
 }
