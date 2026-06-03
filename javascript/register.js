98fcae69b5d19984651930baac01dcc2f2942948
belmanal <manalbel710@gmail.com>
2026-06-03 19:56:03 +0100

    Update register.js

--- javascript/register.js
+++ javascript/register.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("registerForm");
 const langue = document.getElementById("langue");
 
+
 // Traductions
 const texts = {
     fr: {
@@ -54,42 +55,88 @@ function updateLanguage(lang) {
 
 // message + INSCRIPTION
 form.addEventListener("submit", function (e) {
+
     e.preventDefault();
+
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("passwordError").textContent = "";
 
     const lang = langue.value || "fr";
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    // 🔥 AJOUT LOCALSTORAGE (nouveau فقط)
     const user = {
-        name: name,
-        email: email,
-        password: password
+        name: document.getElementById("name").value.trim(),
+        email: document.getElementById("email").value.trim(),
+        password: document.getElementById("password").value.trim()
     };
 
-    localStorage.setItem("user", JSON.stringify(user));
+    const nameRegex = /^[A-Za-zÀ-ÿ\u0600-\u06FF\s]{3,}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{6,}$/;
 
-    // message succès
+    if (!nameRegex.test(user.name)) {
+
+        document.getElementById("nameError").textContent =
+            "Le prénom doit contenir seulement des lettres, 3 lettres minimum.";
+
+        return;
+    }
+
+    if (!emailRegex.test(user.email)) {
+
+        document.getElementById("emailError").textContent =
+            "Adresse email invalide.";
+
+        return;
+    }
+
+    if (!passwordRegex.test(user.password)) {
+
+        document.getElementById("passwordError").textContent =
+            "6 caractères minimum, 1 majuscule et 1 chiffre.";
+
+        return;
+    }
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const exist = users.find(
+        u => u.email === user.email
+    );
+
+    if (exist) {
+
+        document.getElementById("emailError").textContent =
+            "Cet email est déjà utilisé.";
+
+        return;
+    }
+
+    users.push(user);
+
+    localStorage.setItem(
+        "users",
+        JSON.stringify(users)
+    );
+
     const msg = document.createElement("div");
+
     msg.className = "success";
-msg.style.background = "#d4edda";
-msg.style.color = "#155724";
-msg.style.padding = "12px";
-msg.style.borderRadius = "20px";
-msg.style.marginTop = "10px";
-msg.style.textAlign = "center";
-msg.style.fontWeight = "bold";
-msg.style.border = "2px solid #28a745";
-msg.style.boxShadow = "0 4px 12px rgba(0,0,0,0.15)";
-msg.style.display = "inline-block";
+    msg.style.background = "#d4edda";
+    msg.style.color = "#155724";
+    msg.style.padding = "12px";
+    msg.style.borderRadius = "20px";
+    msg.style.marginTop = "10px";
+    msg.style.textAlign = "center";
+    msg.style.fontWeight = "bold";
+    msg.style.border = "2px solid #28a745";
+
     msg.textContent = texts[lang].success;
 
     form.appendChild(msg);
 
-    // redirection login
     setTimeout(() => {
         window.location.href = "login.html";
-    }, 4000);
+    }, 2000);
+
 });
